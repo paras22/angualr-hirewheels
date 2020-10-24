8d72e56efc9ad4fe05b376dcfa484aae7d97b6ba
“PARAS” <PARASDUA20@YAHOO.COM>
2020-10-24 23:37:53 +0530

    checkpoint 4

---
+++ tm-car-renting-app-frontend-master-master/src/app/shared/models/vehicledetail.ts
@@ -0,0 +1,15 @@
+export class VehicleDetail {
+    Vehicle_id: number;
+    Vehicle_model: string;
+    Vehicle_ownerId: number;
+    Vehicle_OwnerName: string;
+    Vehicle_number: number;
+    Color: string;
+    Fuel_Type: string;
+    locationId: number;
+    CarImageUrl: string;
+    CostPerHour: number;
+    ActivityId: number;
+    Request_StatusId: number;
+    Vehicle_SubCategoryId: number;
+}
